e4e5211599cf3d5c28d564b28b0e0f4433f76389
Yury Andreev <Yury.Andreev@ru.ibm.com>
2016-11-05 22:59:02 +0300

    add migration scripts

---
+++ activity-rest/src/main/resources/db/migration/V1_5.11.2016_11.10__Create_stream_table.sql
@@ -0,0 +1,5 @@
+create table Stream (
+    ID int not null,
+    NAME varchar(128) not null,
+    MEMO varchar(128) not null
+);
---
+++ activity-rest/src/main/resources/db/migration/V1_5.11.2016_11_12__Create_action_table.sql
@@ -0,0 +1,7 @@
+create table Action (
+  ID int not null,
+  SUMMARY varchar(512) not null,
+  MEMO varchar(1024) not null,
+  WHO varchar(128) not null,
+  STATE varchar(128) not null
+);
